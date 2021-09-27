13c1ef783d0eb92ee1231d4caf4db86c23bd8256
Adexandria <adeolaaderibigbe09@gmail.com>
2021-09-27 19:46:16 +0100

    created data db context

---
+++ MusicPlatform/Services/DataDb.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicPlatform.Model.Library;
+using MusicPlatform.Model.User.Profile;
+
+namespace MusicPlatform.Services
+{
+    public class DataDb :DbContext
+    {
+        public DataDb(DbContextOptions<DataDb> options):base(options)
+        {
+
+        }
+        public DbSet<CreditModel> SongCredits { get; set; }
+        public DbSet<SongModel> Songs { get; set; }
+        public DbSet<SongImage> SongImages { get; set; }
+        public DbSet<UserLibrary> Libraries { get; set; }
+        public DbSet<UserImage> UserImages { get; set; }
+        public DbSet<FollowingModel> Followings { get; set; }
+        public DbSet<UserProfile> Profiles { get; set; }
+    }
+}
