60c1080c671bde108cd5b44139402c7527faf0d1
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-04 15:09:11 +0100

    user services

---
+++ MusicPlatform/Services/UserDetail.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MusicPlatform.Model.User;
+using System;
+using System.Threading.Tasks;
+
+namespace MusicPlatform.Services
+{
+    public class UserDetail
+    {
+        private readonly UserManager<UserModel> userManager;
+        public UserDetail(UserManager<UserModel> userManager)
+        {
+            this.userManager = userManager ?? throw new NullReferenceException(nameof(userManager));
+        }
+        public async Task<string> GetUserId(string username)
+        {
+           UserModel current = await userManager.FindByNameAsync(username);
+            return current.Id;
+        }
+        public async Task<bool> isVerified(string username)
+        {
+            UserModel current = await userManager.FindByNameAsync(username);
+            return current.Verified;
+        }
+    }
+}
