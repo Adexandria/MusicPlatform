2f23d85f4092ae7248f548b795c259f052d47f98
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-06 10:00:23 +0100

    blob interface

---
+++ MusicPlatform/Services/IBlob.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Text_Speech.Services
+{
+    public  interface IBlob
+    {
+        Task Upload(IFormFile model);
+        Uri GetUri(string file);
+
+        Task DeleteImage(string url);
+      //  Task UploadFile(FileStream file);
+      
+       // Task UploadStream(Stream model);
+        
+       /* Task<byte[]> Download(IFormFile file);
+        Task<string[]> DownloadFile(string file);*/
+    }
+}
