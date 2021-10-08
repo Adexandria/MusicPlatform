c9a6c848dab3afe1ddb08e5b75156670ecfcc7d0
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-08 07:32:02 +0100

    user repository

--- MusicPlatform/Services/UserDetail.cs
+++ MusicPlatform/Services/UserDetail.cs
@@ -1,31 +1,37 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using MusicPlatform.Model.User;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MusicPlatform.Services
 {
-    public class UserDetail
+    public class UserDetail : IUser
     {
-        private readonly UserManager<UserModel> userManager;
-        public UserDetail(UserManager<UserModel> userManager)
+        private readonly DataDb db;
+        public UserDetail( DataDb db)
         {
-            this.userManager = userManager ?? throw new NullReferenceException(nameof(userManager));
+            this.db = db ?? throw new NullReferenceException(nameof(db));
         }
         public async Task<string> GetUserId(string username)
         {
-           UserModel current = await userManager.FindByNameAsync(username);
-            return current.Id;
+            var currentUser = await GetUser(username);
+            return currentUser.Id;
+        }
+        public async Task<bool> IsVerified(string username)
+        {
+            var currentUser = await GetUser(username);
+            return currentUser.Verified;
         }
         public async Task<UserModel> GetUser(string username)
         {
-            UserModel currentUser = await userManager.FindByNameAsync(username);
+            var currentUser = await db.UserModel.Where(s => s.UserName == username).AsNoTracking().FirstOrDefaultAsync();
+            if (currentUser != null)
+            {
+                db.Entry(currentUser).State = EntityState.Detached;
+            }
             return currentUser;
-        }
-        public async Task<bool> isVerified(string username)
-        {
-            UserModel current = await userManager.FindByNameAsync(username);
-            return current.Verified;
         }
     }
 }
