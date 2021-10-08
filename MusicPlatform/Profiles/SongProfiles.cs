0323387e7021266d094d02751633d8429b9dc250
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-08 09:09:15 +0100

    created profile for song model

---
+++ MusicPlatform/Profiles/SongProfiles.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MusicPlatform.Model.Library;
+using MusicPlatform.Model.Library.DTO;
+using MusicPlatform.Model.User.Profile.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicPlatform.Profiles
+{
+    public class SongProfiles :Profile
+    {
+        public SongProfiles()
+        {
+            CreateMap<SongModel, SongsDTO>().ForMember(s => s.SongName, opt => opt.MapFrom(s => s.SongName));
+            CreateMap<SongModel, SongDTO>().ForMember(s => s.SongName, opt => opt.MapFrom(s => s.SongName))
+                .ForMember(s => s.SongURL, opt => opt.MapFrom(s => s.SongImage))
+                .ForMember(s => s.Download, opt => opt.MapFrom(s => s.Download))
+                .ForMember(s => s.Credit, opt => opt.MapFrom(s => s.CreditModel))
+                .ForMember(s => s.ReleasedDate, opt => opt.MapFrom(s => s.ReleasedDate));
+        }
+    }
+}
