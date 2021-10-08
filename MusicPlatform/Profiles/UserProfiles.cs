c465e4a4fb18ff46914668e1f232048f48d72e98
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-08 07:30:40 +0100

    added mapping properties for artistDTO and UserDTO

--- MusicPlatform/Profiles/UserProfiles.cs
+++ MusicPlatform/Profiles/UserProfiles.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using MusicPlatform.Model.User;
 using MusicPlatform.Model.User.Login;
+using MusicPlatform.Model.User.Profile;
+using MusicPlatform.Model.User.Profile.ProfileDTO;
 using MusicPlatform.Model.User.SignUpDTO;
+using System.Linq;
 
 namespace MusicPlatform.Profiles
 {
@@ -13,7 +16,17 @@ namespace MusicPlatform.Profiles
             CreateMap<SignUpArtist, UserModel>().ForMember(s => s.PasswordHash, opt => opt.MapFrom(s => s.Password))
                 .ForMember(s=>s.UserName,opt=>opt.MapFrom(s=>s.ArtistName));
             CreateMap<Login, UserModel>().ForMember(s => s.PasswordHash, opt => opt.MapFrom(s => s.Password));
+
+            CreateMap<UserProfile, ArtistProfileDTO>().ForPath(s => s.Artistname, opt => opt.MapFrom(s => s.User.UserName))
+               .ForMember(s => s.Followers, opt => opt.MapFrom(s => s.Followers.Count()))
+                .ForMember(s => s.Following, opt => opt.MapFrom(s => s.Followings.Count()))
+                .ForMember(s => s.Songs, opt => opt.MapFrom(s => s.Songs))
+                .ForPath(s => s.ImageUrl, opt => opt.MapFrom(s => s.UserImage.ImageUrl));
             
+            CreateMap<UserProfile, UserProfileDTO>().ForPath(s => s.Username, opt => opt.MapFrom(s => s.User.UserName))
+                .ForMember(s => s.Following, opt => opt.MapFrom(s => s.Followings.Count()))
+                .ForMember(s => s.Followers, opt => opt.MapFrom(s => s.Followers.Count()))
+                .ForPath(s => s.ImageUrl, opt => opt.MapFrom(s => s.UserImage.ImageUrl));
         }
     }
 }
