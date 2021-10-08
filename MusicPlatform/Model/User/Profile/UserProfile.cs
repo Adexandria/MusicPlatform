294db47565af9f1deb8c4d3505cd14dba370f8e8
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-08 07:30:12 +0100

    added following model and song model property

--- MusicPlatform/Model/User/Profile/UserProfile.cs
+++ MusicPlatform/Model/User/Profile/UserProfile.cs
@@ -11,12 +11,14 @@ namespace MusicPlatform.Model.User.Profile
     {
         [Key]
         public Guid ProfileId { get; set; }
-        [ForeignKey("Id")]
+        [ForeignKey("AspNetUsersId")]
         public string UserId { get; set; }
 
         public virtual UserModel User { get; set; }
         public virtual UserImage UserImage { get; set; }
-        public virtual List<FollowingModel> Following { get; set; }
+        public virtual List<FollowingModel> Followings { get; set; }
+        public virtual List<FollowingModel> Followers { get; set; }
+        public virtual List<SongModel> Songs { get; set; }
 
     }
 }
