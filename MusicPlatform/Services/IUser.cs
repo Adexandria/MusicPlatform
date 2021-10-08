57c6b7495e99569deaa2b894fb70930abcebb1de
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-08 07:31:06 +0100

    User interface

---
+++ MusicPlatform/Services/IUser.cs
@@ -0,0 +1,16 @@
+﻿using MusicPlatform.Model.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicPlatform.Services
+{
+    public interface IUser 
+    {
+        Task<UserModel> GetUser(string username);
+        Task<bool> IsVerified(string username);
+        Task<string> GetUserId(string username);
+
+    }
+}
