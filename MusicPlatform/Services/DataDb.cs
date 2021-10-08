9672a50f3acb1d1ce072b724285c74c56ee43a81
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-08 07:30:57 +0100

    added IUser interface

--- MusicPlatform/Services/DataDb.cs
+++ MusicPlatform/Services/DataDb.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MusicPlatform.Model.Library;
+using MusicPlatform.Model.User;
 using MusicPlatform.Model.User.Profile;
 
 namespace MusicPlatform.Services
@@ -17,5 +18,11 @@ namespace MusicPlatform.Services
         public DbSet<UserImage> UserImages { get; set; }
         public DbSet<FollowingModel> Followings { get; set; }
         public DbSet<UserProfile> Profiles { get; set; }
+        public DbSet<UserModel> UserModel { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FollowingModel>().HasOne(s => s.FollowerProfile).WithMany(s => s.Followings).HasForeignKey(s => s.ProfileId);
+            
+        }
     }
 }
