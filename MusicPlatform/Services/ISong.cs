af110ee486e290f6e5ca8a0ea53f60fc654cb992
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-08 09:10:08 +0100

    added search song method

--- MusicPlatform/Services/ISong.cs
+++ MusicPlatform/Services/ISong.cs
@@ -10,9 +10,12 @@ namespace MusicPlatform.Services
     {
         Task AddSong(SongModel song,string artistId);
         Task<SongModel> GetSong(string artistId,Guid songId);
+        IEnumerable<SongModel> GetSong(string name);
         Task UpdateSong(SongModel song,Guid songId,string artistId);
         Task DeleteSong(Guid songId, string artistId);
-
+      //  IEnumerable<SongModel> GetSongs { get; }
+       
+       // Task<SongModel> GetSongById(Guid songId);
         Task AddCredit(CreditModel credit);
         Task UpdateCredit(CreditModel credit, Guid songId);
         Task DeleteCredit(Guid creditId);
