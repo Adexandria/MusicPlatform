6aac86ad78089c7aa9872be9bc9fb78fb5c00cd5
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-15 21:27:56 +0100

    removed follow,unfollow and search songs method

--- MusicPlatform/Controllers/SearchController.cs
+++ MusicPlatform/Controllers/SearchController.cs
@@ -18,16 +18,12 @@ namespace MusicPlatform.Controllers
     [ApiController]
     public class SearchController : ControllerBase
     {
-        private readonly IUserProfile _profile;
-        private readonly ISong _song;
         private readonly IMapper mapper;
         private readonly IUser userDetail;
-        public SearchController(IUserProfile _profile, IMapper mapper, IUser userDetail, ISong _song)
+        public SearchController( IMapper mapper, IUser userDetail)
         {
-            this._profile = _profile;
             this.mapper = mapper;
             this.userDetail = userDetail;
-            this._song = _song;
         }
        
         
@@ -51,49 +47,7 @@ namespace MusicPlatform.Controllers
 
             }
         }
-        /*[Authorize("BasicAuthentication")]
-       [HttpGet("users")]
-       public ActionResult<IEnumerable<UsersDTO>> GetUsers([FromRoute] string username)
-       {
-           try
-           {
-               var currentUser = userDetail.GetUser(username).Result;
-               if (currentUser == null)
-               {
-                   return NotFound("User not found");
-               }
-               var artists = userDetail.GetUsers;
-               var mappedArtists = mapper.Map<IEnumerable<UsersDTO>>(artists);
-               return Ok(mappedArtists);
-           }
-           catch (Exception e)
-           {
-               return BadRequest(e.Message);
-
-           }
-       
-        /*        [Authorize("BasicAuthentication")]
-                [HttpGet("artists")]
-                public ActionResult<IEnumerable<ArtistsDTO>> GetArtists([FromRoute] string username)
-                {
-                    try
-                    {
-                        var currentUser =  userDetail.GetUser(username).Result;
-                        if (currentUser == null)
-                        {
-                            return NotFound("User not found");
-                        }
-                        var artists = userDetail.GetArtists;
-                        var mappedArtists = mapper.Map<IEnumerable<ArtistsDTO>>(artists);
-                        return Ok(mappedArtists);
-                    }
-                    catch (Exception e)
-                    {
-                        return BadRequest(e.Message);
-
-                    }
-                } */
-        //[Authorize("BasicAuthentication")]
+    
         [HttpGet("artists/{artistname}")]
         public ActionResult<IEnumerable<ArtistsDTO>> SearchArtist([FromRoute] string username,string artistname)
         {
@@ -115,71 +69,9 @@ namespace MusicPlatform.Controllers
             }
         }
 
-        [AllowAnonymous]
-        [HttpGet("songs")]
-        public ActionResult<IEnumerable<SongsDTO>> SearchSong([FromRoute] string username,string songName)
-        {
-            try
-            {
-                var currentUser = userDetail.GetUser(username).Result;
-                if (currentUser == null)
-                {
-                    return NotFound("User not found");
-                }
-                var songs = _song.GetSong(songName);
-                var mappedSongs = mapper.Map<IEnumerable<SongsDTO>>(songs);
-                return Ok(mappedSongs);
-            }
-            catch (Exception e)
-            {
+        
 
-                return BadRequest(e.Message);
-            }
-           
-        }
-
-        [Authorize("BasicAuthentication")]
-        [HttpPost("Follow/{follower}")]
-        public async Task<ActionResult> FollowArtist([FromRoute] string username, string follower)
-        {
-            var currentUser = await userDetail.GetUser(username);
-            if (currentUser == null)
-            {
-                return NotFound("User not found"); 
-            }
-            var currentFollower = await userDetail.GetUser(follower);
-            if (currentFollower == null)
-            {
-                return NotFound("artist not found");
-            }
-            if (await _profile.IsFollowing(currentUser.Id, currentFollower.Id))
-            {
-                return BadRequest("This User is being followed by you");
-            }
-            await _profile.Follow(username, follower);
-            return Ok($"Following {follower}");
-        }
-
-        [Authorize("BasicAuthentication")]
-        [HttpDelete("Follow/{follower}")]
-        public async Task<ActionResult> UnFollowArtist([FromRoute] string username, string follower)
-        {
-            var currentUser = await userDetail.GetUser(username);
-            if (currentUser == null)
-            {
-                return NotFound("User not found");
-            }
-            var currentFollower = await userDetail.GetUser(follower);
-            if (currentFollower == null)
-            {
-                return NotFound("artist not found");
-            }
-            if (await _profile.IsFollowing(currentUser.Id, currentFollower.Id))
-            {
-                await _profile.UnFollow(currentUser.Id, currentFollower.Id);
-                return Ok($"Unfollowing {follower}");
-            }
-            return BadRequest("Unsuccessful, the follower isn't being followed ");
-        }
+      
+     
     }
 }
