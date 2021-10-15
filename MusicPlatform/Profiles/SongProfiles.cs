863baf22ae8780818a27daced97c18de3d9ffa21
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-15 09:01:57 +0100

    added maping properties for song update and create, credit create and update

--- MusicPlatform/Profiles/SongProfiles.cs
+++ MusicPlatform/Profiles/SongProfiles.cs
@@ -14,11 +14,17 @@ namespace MusicPlatform.Profiles
         public SongProfiles()
         {
             CreateMap<SongModel, SongsDTO>().ForMember(s => s.SongName, opt => opt.MapFrom(s => s.SongName));
+            CreateMap<SongCreate, SongModel>();
+            CreateMap<SongUpdate, SongModel>();
             CreateMap<SongModel, SongDTO>().ForMember(s => s.SongName, opt => opt.MapFrom(s => s.SongName))
-                .ForMember(s => s.SongURL, opt => opt.MapFrom(s => s.SongImage))
+                .ForMember(s => s.SongURL, opt => opt.MapFrom(s => s.SongUrl))
                 .ForMember(s => s.Download, opt => opt.MapFrom(s => s.Download))
+                .ForPath(s=>s.ImageUrl,opt=>opt.MapFrom(s=>s.SongImage.ImageUrl))
                 .ForMember(s => s.Credit, opt => opt.MapFrom(s => s.CreditModel))
                 .ForMember(s => s.ReleasedDate, opt => opt.MapFrom(s => s.ReleasedDate));
+
+            CreateMap<CreditCreate, CreditModel>();
+            CreateMap<CreditUpdate, CreditModel>();
         }
     }
 }
