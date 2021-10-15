887b288d4b25059aee53ae79cc8b34139bc626dc
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-15 08:59:57 +0100

    added a string image url

--- MusicPlatform/Model/Library/DTO/SongDTO.cs
+++ MusicPlatform/Model/Library/DTO/SongDTO.cs
@@ -6,6 +6,7 @@ namespace MusicPlatform.Model.User.Profile.DTO
     {
         public string SongName { get; set; }
         public string SongURL { get; set; }
+        public string ImageUrl { get; set; }
         public  DateTime ReleasedDate { get; set; }
         public CreditModelDTO Credit { get; set; }
         public int Download { get; set; }
