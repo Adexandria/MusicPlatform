4b241cb57d6775e6748c565f6d0751fc58fd99b3
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-15 21:28:42 +0100

    created controller to search songs

---
+++ MusicPlatform/Controllers/SongsController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicPlatform.Model.Library.DTO;
+using MusicPlatform.Model.User.Profile.DTO;
+using MusicPlatform.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class SongsController : ControllerBase
+    {
+        private readonly ISong _song;
+        private readonly IMapper mapper;
+        private readonly IUser userDetail;
+        public SongsController(ISong _song,IMapper mapper, IUser userDetail)
+        {
+            this._song = _song;
+            this.mapper = mapper;
+            this.userDetail = userDetail;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<SongsDTO>> GetSongs(string songName)
+        {
+            try
+            {
+                var songs = _song.GetSong(songName);
+                var mappedSongs = mapper.Map<IEnumerable<SongsDTO>>(songs);
+                return Ok(mappedSongs);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+
+        }
+        [HttpGet("{artist}/{songName}")]
+        public async Task<ActionResult<SongDTO>> GetSong(string artist,string songName)
+        {
+            try
+            {
+                var currentUser = await userDetail.GetUser(artist);
+                if (currentUser == null)
+                {
+                    return NotFound("User not found");
+                }
+
+                var song = await _song.GetSong(artist,songName);
+                if(song == null)
+                {
+                    return NotFound("Song not found");
+                }
+                var mappedSongs = mapper.Map<SongDTO>(song);
+                return Ok(mappedSongs);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+
+        }
+    }
+}
