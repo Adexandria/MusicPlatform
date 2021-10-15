04ca1d4113c3750fae9b86c919ee4af07a187a82
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-15 08:59:29 +0100

    modelto update image

---
+++ MusicPlatform/Model/Library/DTO/ImageUpdate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicPlatform.Model.Library.DTO
+{
+    public class ImageUpdate
+    {
+        public string ImageUrl { get; set; }
+    }
+}
