fa7670e98eb47b4328f9990d4c22f9afdfa95d69
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-16 20:17:38 +0100

    created userlibraries dto

---
+++ MusicPlatform/Model/Library/DTO/UserLibrariesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicPlatform.Model.Library.DTO
+{
+    public class UserLibrariesDTO
+    {
+        public string Artist { get; set; }
+        public string SongName { get; set; }
+
+    }
+}
