c50060d6d3d60286ec1e918f29f464cf3ba57d8a
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-26 19:41:45 +0100

    added try and catch handler

--- MusicPlatform/Controllers/SongController.cs
+++ MusicPlatform/Controllers/SongController.cs
@@ -7,6 +7,7 @@ using MusicPlatform.Model.Library;
 using MusicPlatform.Model.Library.DTO;
 using MusicPlatform.Model.User;
 using MusicPlatform.Services;
+using System;
 using System.Threading.Tasks;
 using Text_Speech.Services;
 
@@ -34,111 +35,164 @@ namespace MusicPlatform.Controllers
         [HttpPost]
         public async Task<IActionResult> UploadSong(string username,[FromForm]SongCreate newSong)
         {
-            
-            var currentUser = await userDetail.GetUser(username);
-            if (currentUser == null)
+            try
             {
-                return NotFound("Username not found");
+                var currentUser = await userDetail.GetUser(username);
+                if (currentUser == null)
+                {
+                    return NotFound("Username not found");
+                }
+                await _blob.Upload(newSong.Song);
+                string url = _blob.GetUri(newSong.Song.FileName).ToString();
+                var mappedSong = mapper.Map<SongModel>(newSong);
+                mappedSong.SongUrl = url;
+                await _song.AddSong(mappedSong, username);
+                return Ok("Success");
             }
-            await _blob.Upload(newSong.Song);
-            string url = _blob.GetUri(newSong.Song.FileName).ToString();
-            var mappedSong = mapper.Map<SongModel>(newSong);
-            mappedSong.SongUrl = url;
-            await _song.AddSong(mappedSong, username);
-            return Ok("Success");
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+                
+            }
+            
         }
 
         [HttpPut("{songName}")]
         public async Task<IActionResult> UpdateSong(string username,string songName, [FromForm] SongUpdate updatedSong)
         {
-            var currentUser = await userDetail.GetUser(username);
-            if (currentUser == null)
+            try
             {
-                return NotFound("Username not found");
+                var currentUser = await userDetail.GetUser(username);
+                if (currentUser == null)
+                {
+                    return NotFound("Username not found");
+                }
+                var currentSong = await _song.GetSong(username, songName);
+                if (currentSong == null)
+                {
+                    return NotFound();
+                }
+                await _blob.Upload(updatedSong.Song);
+                string url = _blob.GetUri(updatedSong.Song.FileName).ToString();
+                var mappedSong = mapper.Map<SongModel>(updatedSong);
+                mappedSong.SongUrl = url;
+                await _song.UpdateSong(mappedSong, songName, username);
+                return Ok("Success");
             }
-            var currentSong = await _song.GetSong(username, songName);
-            if (currentSong == null)
+            catch (Exception e)
             {
-                return NotFound();
+                return BadRequest(e.Message);
+
             }
-            await _blob.Upload(updatedSong.Song);
-            string url = _blob.GetUri(updatedSong.Song.FileName).ToString();
-            var mappedSong = mapper.Map<SongModel>(updatedSong);
-            mappedSong.SongUrl = url;
-            await _song.UpdateSong(mappedSong, songName, username);
-            return Ok("Success");
+           
         }
 
         [HttpDelete("{songName}")]
         public async Task<IActionResult> DeleteSong(string username, string songName)
         {
-            var currentUser = await userDetail.GetUser(username);
-            if (currentUser == null)
+            try
             {
-                return NotFound("Username not found");
+                var currentUser = await userDetail.GetUser(username);
+                if (currentUser == null)
+                {
+                    return NotFound("Username not found");
+                }
+                var currentSong = await _song.GetSong(username, songName);
+                if (currentSong == null)
+                {
+                    return NotFound();
+                }
+                await _song.DeleteSong(username, songName);
+                return Ok("Success");
             }
-            var currentSong = await _song.GetSong(username, songName);
-            if (currentSong == null)
+            catch (Exception e)
             {
-                return NotFound();
+                return BadRequest(e.Message);
+
             }
-            await _song.DeleteSong(username, songName);
-            return Ok("Success");
+            
         }
 
 
         [HttpPost("{songName}/image")]
         public async Task<IActionResult> AddSongImage([FromRoute]string username,[FromRoute]string songName,IFormFile image)
         {
-            var currentUser = await userDetail.GetUser(username);
-            if (currentUser == null)
+            try
             {
-                return NotFound("Username not found");
+                var currentUser = await userDetail.GetUser(username);
+                if (currentUser == null)
+                {
+                    return NotFound("Username not found");
+                }
+                var currentSong = await _song.GetSong(username, songName);
+                if (currentSong == null)
+                {
+                    return NotFound("Song doesn't exist");
+                }
+                await _blob.Upload(image);
+                string url = _blob.GetUri(image.FileName).ToString();
+                await _song.AddImage(url, currentSong.SongId);
+                return Ok("Success");
             }
-            var currentSong = await _song.GetSong(username, songName);
-            if(currentSong == null)
+            catch (Exception e)
             {
-                return NotFound("Song doesn't exist");
+                return BadRequest(e.Message);
+
             }
-            await _blob.Upload(image);
-            string url = _blob.GetUri(image.FileName).ToString();
-            await _song.AddImage(url,currentSong.SongId);
-            return Ok("Success");
+            
         }
         [HttpPut("{songName}/image")]
         public async Task<IActionResult> UpdateSongImage([FromRoute] string username, [FromRoute] string songName, IFormFile image)
         {
-            var currentUser = await userDetail.GetUser(username);
-            if (currentUser == null)
+            try
             {
-                return NotFound("Username not found");
+                var currentUser = await userDetail.GetUser(username);
+                if (currentUser == null)
+                {
+                    return NotFound("Username not found");
+                }
+                var currentSong = await _song.GetSong(username, songName);
+                if (currentSong == null)
+                {
+                    return NotFound("Song doesn't exist");
+                }
+                await _blob.Upload(image);
+                string url = _blob.GetUri(image.FileName).ToString();
+                await _song.UpdateImage(url, currentSong.SongId);
+                return Ok("Success");
             }
-            var currentSong = await _song.GetSong(username, songName);
-            if (currentSong == null)
+            catch (Exception e)
             {
-                return NotFound("Song doesn't exist");
+                return BadRequest(e.Message);
+
             }
-            await _blob.Upload(image);
-            string url = _blob.GetUri(image.FileName).ToString();
-            await _song.UpdateImage(url, currentSong.SongId);
-            return Ok("Success");
+           
         }
 
         [HttpDelete("{songName}/image")]
         public async Task<IActionResult> DeleteSongImage([FromRoute] string username, [FromRoute] string songName)
         {
-            var currentUser = await userDetail.GetUser(username);
-            if (currentUser == null)
+            try
             {
-                return NotFound("Username not found");
+                var currentUser = await userDetail.GetUser(username);
+                if (currentUser == null)
+                {
+                    return NotFound("Username not found");
+                }
+                var currentSong = await _song.GetSong(username, songName);
+                if (currentSong == null)
+                {
+                    return NotFound("Song doesn't exist");
+                }
+                await _song.DeleteImage(currentSong.SongId);
+                return Ok("Success");
             }
-            var currentSong = await _song.GetSong(username, songName);
-            if (currentSong == null)
+            catch (Exception e)
             {
-                return NotFound("Song doesn't exist");
+                return BadRequest(e.Message);
+
             }
-            await _song.DeleteImage(currentSong.SongId);
-            return Ok("Success");
+            
         }
 
 
