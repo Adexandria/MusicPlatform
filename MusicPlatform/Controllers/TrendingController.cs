8d8c2f4a8ee3550a57937e90fdbb908e63520fa7
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-26 19:45:53 +0100

    removed unused libraries

--- MusicPlatform/Controllers/TrendingController.cs
+++ MusicPlatform/Controllers/TrendingController.cs
@@ -1,13 +1,10 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MusicPlatform.Model.Library.DTO;
 using MusicPlatform.Services;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace MusicPlatform.Controllers
 {
