c051b47445728502680c41ef3542a6c63ecf79bb
Adexandria <adeolaaderibigbe09@gmail.com>
2021-10-26 19:48:27 +0100

    added try and catch block and removed unused libraries

--- MusicPlatform/Controllers/SongCreditController.cs
+++ MusicPlatform/Controllers/SongCreditController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using MusicPlatform.Model.Library;
 using MusicPlatform.Model.Library.DTO;
 using MusicPlatform.Services;
+using System;
 using System.Threading.Tasks;
 
 namespace MusicPlatform.Controllers
@@ -26,54 +27,83 @@ namespace MusicPlatform.Controllers
         [HttpPost]
         public async Task<IActionResult> AddSongCredit([FromRoute] string username, [FromRoute] string songName, CreditCreate newCredit)
         {
-            var currentUser = await userDetail.GetUser(username);
-            if (currentUser == null)
+            try
             {
-                return NotFound("Username not found");
+                var currentUser = await userDetail.GetUser(username);
+                if (currentUser == null)
+                {
+                    return NotFound("Username not found");
+                }
+                var currentSong = await _song.GetSong(username, songName);
+                if (currentSong == null)
+                {
+                    return NotFound("Song doesn't exist");
+                }
+                var mappedCredit = mapper.Map<CreditModel>(newCredit);
+                await _song.AddCredit(currentSong.SongId, mappedCredit);
+                return Ok("Success");
             }
-            var currentSong = await _song.GetSong(username, songName);
-            if (currentSong == null)
+            catch (Exception e)
             {
-                return NotFound("Song doesn't exist");
+                return BadRequest(e.Message);
+
             }
-            var mappedCredit = mapper.Map<CreditModel>(newCredit);
-            await _song.AddCredit(currentSong.SongId, mappedCredit);
-            return Ok("Success");
+            
         }
 
         [HttpPut]
         public async Task<IActionResult> UpdateSongCredit([FromRoute] string username, [FromRoute] string songName, CreditUpdate updateCredit)
         {
-            var currentUser = await userDetail.GetUser(username);
-            if (currentUser == null)
+
+            try
             {
-                return NotFound("Username not found");
+                var currentUser = await userDetail.GetUser(username);
+                if (currentUser == null)
+                {
+                    return NotFound("Username not found");
+                }
+                var currentSong = await _song.GetSong(username, songName);
+                if (currentSong == null)
+                {
+                    return NotFound("Song doesn't exist");
+                }
+                var mappedCredit = mapper.Map<CreditModel>(updateCredit);
+                await _song.UpdateCredit(mappedCredit, currentSong.SongId);
+                return Ok("Success");
             }
-            var currentSong = await _song.GetSong(username, songName);
-            if (currentSong == null)
+            catch (Exception e)
             {
-                return NotFound("Song doesn't exist");
+                return BadRequest(e.Message);
+
             }
-            var mappedCredit = mapper.Map<CreditModel>(updateCredit);
-            await _song.UpdateCredit(mappedCredit, currentSong.SongId);
-            return Ok("Success");
+            
         }
 
         [HttpDelete]
         public async Task<IActionResult> DeleteSongCredit([FromRoute] string username, [FromRoute] string songName)
         {
-            var currentUser = await userDetail.GetUser(username);
-            if (currentUser == null)
+
+            try
             {
-                return NotFound("Username not found");
+                var currentUser = await userDetail.GetUser(username);
+                if (currentUser == null)
+                {
+                    return NotFound("Username not found");
+                }
+                var currentSong = await _song.GetSong(username, songName);
+                if (currentSong == null)
+                {
+                    return NotFound("Song doesn't exist");
+                }
+                await _song.DeleteCredit(currentSong.SongId);
+                return Ok("Success");
             }
-            var currentSong = await _song.GetSong(username, songName);
-            if (currentSong == null)
+            catch (Exception e)
             {
-                return NotFound("Song doesn't exist");
+                return BadRequest(e.Message);
+
             }
-            await _song.DeleteCredit(currentSong.SongId);
-            return Ok("Success");
+            
         }
     }
 }
